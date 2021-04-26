6d1a07d630db087953e24059bfa981fc17b8adf6
Christian-Martinez <christ96mart@gmail.com>
2021-04-25 22:09:54 -0400

    component weather aplicando TDD, context.provider para react icons

---
+++ src/components/Weather/Weather.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Typography } from '@material-ui/core';
+import {
+  WiCloud,
+  WiDayCloudy,
+  WiDaySunny,
+  WiRain,
+  WiSnow,
+  WiRaindrop,
+  WiThunderstorm,
+} from 'react-icons/wi';
+import { IconContext } from 'react-icons';
+
+const stateByName = {
+  clouds: WiDayCloudy,
+  clear: WiDaySunny,
+  rain: WiRain,
+  snow: WiSnow,
+  drizzle: WiRaindrop,
+  thunderstorm: WiThunderstorm,
+};
+
+/* funcion que volvera a renderizzar el componente */
+const renderState = (state) => {
+  /* Componente dinamico empieza con mayuscula */
+  const StateByName = stateByName[state];
+  return <StateByName />;
+};
+
+const Weather = ({ temperature, state }) => {
+  return (
+    <div>
+      <IconContext.Provider value={{ size: '5em' }}>{renderState(state)}</IconContext.Provider>
+      <Typography display='inline' variant='h2'>
+        {temperature}
+      </Typography>
+    </div>
+  );
+};
+
+Weather.propTypes = {
+  temperature: PropTypes.number.isRequired,
+  state: PropTypes.string.isRequired,
+};
+
+export default Weather;
---
+++ src/components/Weather/Weather.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Weather from './Weather';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+// TDD
+test('Weather render sunny', async () => {
+  // AAA Arrange Act Assert
+  /* findByRole extrae un solo rol */
+  const { findByRole } = render(<Weather temperature={10} state='clouds' />);
+
+  const temp = await findByRole('heading');
+
+  expect(temp).toHaveTextContent('10');
+});
