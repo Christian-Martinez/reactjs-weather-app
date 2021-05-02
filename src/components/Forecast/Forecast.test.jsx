062a00bb191ac694a558987359dce9b386f0fafe
Christian-Martinez <christ96mart@gmail.com>
2021-05-02 19:48:58 -0400

    component ForecastChart con biblioteca de graficos Recharts

--- src/components/Forecast/Forecast.test.jsx
+++ src/components/Forecast/Forecast.test.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import Forecast from './Forecast'
-import { render } from '@testing-library/react'
+import React from 'react';
+import Forecast from './Forecast';
+import { render } from '@testing-library/react';
 
 const forecastItemList = [
-	{ hour: 18, state:"clear", temperature:17, weekDay:"Jueves" },
-	{ hour: 6, state:"clouds", temperature:18, weekDay:"Viernes" },
-	{ hour: 12, state:"drizzle", temperature:18, weekDay:"Viernes" },
-	{ hour: 18, state:"clouds", temperature:19, weekDay:"Viernes" },
-	{ hour: 6, state:"rain", temperature:17, weekDay:"Sábado" },
-	{ hour: 12, state:"rain", temperature:17, weekDay:"Sábado" }, 
-]
+  { hour: 18, state: 'clear', temperature: 17, weekDay: 'Jueves' },
+  { hour: 6, state: 'clouds', temperature: 18, weekDay: 'Viernes' },
+  { hour: 12, state: 'drizzle', temperature: 18, weekDay: 'Viernes' },
+  { hour: 18, state: 'clouds', temperature: 19, weekDay: 'Viernes' },
+  { hour: 6, state: 'rain', temperature: 17, weekDay: 'Sábado' },
+  { hour: 12, state: 'rain', temperature: 17, weekDay: 'Sábado' },
+];
 
 test('Forecast render', async () => {
   /* findAllByTestId nos va a permitir encontrar cada item con esa marca */
-  const { findAllByTestId } = render(<Forecast forecastItemList={forecastItemList} />)
+  const { findAllByTestId } = render(<Forecast forecastItemList={forecastItemList} />);
 
-  const forecastItems = await findAllByTestId("forecast-item-container")
+  const forecastItems = await findAllByTestId('forecast-item-container');
 
-  expect(forecastItems).toHaveLength(6)
-})
+  expect(forecastItems).toHaveLength(6);
+});
